8033ffbb333d8376dcd27960f9dfa848e3b13dc0
Mustafa Faisal <122450274+MustafaFaisal-778@users.noreply.github.com>
2023-02-15 13:54:19 +0500

    Add files via upload

---
+++ JS chap 17to20/app.js
@@ -0,0 +1,84 @@
+//Question#1 
+// var Arr=[[1,2],[3,4],[5,6]]
+
+//Question#2
+
+//Question#3
+// for (i=1; i<11; i++){
+//     document.write(i+"<br />")
+// }
+
+//Question#4
+// var num1=+prompt("enter number")
+// var length=+prompt("enter length of the table")
+// for(i=1; i<=length; i++){
+//     document.write("<h1>"+num1+" x "+i+" = "+i*num1+"</h1>")
+// }
+
+//Question#5
+// fruits = ["apple", "banana", "mango", "orange","strawberry"]
+// for(i=0;i<=fruits.length;i++){
+//     document.write(fruits[i] +"<br />")
+// }
+
+//Question#6
+// document.write("Counting:")
+// for(i=1;i<=15;i++){
+//     document.write(i+",")
+// }
+
+// document.write("<h1>Reverse:</h1>")
+// for(i=10;i>=1;i--){
+//     document.write(i+",")
+// }
+
+// document.write("<h1>Even:</h1>")
+// for(i=0;i<=20;i=i+2){
+//     document.write(i+",")
+// }
+
+// document.write("<h1>Odd:</h1>")
+// for(i=1;i<=20;i=i+2){
+//     document.write(i+",")
+// }
+
+// document.write("<h1>Series:</h1>")
+// for(i=2;i<=20;i=i+2){
+//     document.write(i+"K, ")
+// }
+
+//Question#7
+// A = ["cake", "apple pie", "cookie", "chips", "patties"]
+// var input=prompt("enter variable")
+// flag=false
+// for(i=0;i<=A.length;i++){
+//     if(input==A[i]){
+       
+//         flag=true
+//     }
+    
+// }
+// if(flag==true){
+//     document.write(input+" is availible at index "+i+"")
+// }
+// else{
+//     document.write(input+" is not availible")
+// }
+
+//Question#8
+// var A = [24, 53, 78, 91, 12]
+// A.sort()
+// console.log(A)
+// console.log("largest number is "+A[A.length-1])
+
+//Question#9
+// A = [24, 53, 78, 91, 12]
+// A.sort()
+// console.log(A)
+// console.log("smallest number is "+A[0])
+
+//Question#10
+// for(i=5;i<=100;i=i+5){
+//     console.log(i)
+// }
+
